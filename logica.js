23e07b75373b6c9a11c69e19941b12a4e763dbe5
luis <tauro_luisito_20@hotmail.com>
2017-09-09 13:57:19 -0500

    prueba5

--- logica.js
+++ logica.js
@@ -1,4 +1,17 @@
+function escribir(){
+  var txtFile = "data.txt";
+  var file = new File(txtFile);
+  var str = "bien y tu?";
+
+  file.open("w"); // open file with write access
+  file.writeln("First line of text");
+  file.writeln("Second line of text " + str);
+  file.write(str);
+  file.close();
+}
+
 $(document).ready(function(){
+  escribir();
   $.get('data.txt', function(data) {
    alert(data);
   }, 'text');
