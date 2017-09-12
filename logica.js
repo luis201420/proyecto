cd2ff097714c448e19d49cd894a67137d8c541a7
luis <tauro_luisito_20@hotmail.com>
2017-09-12 18:45:29 -0500

    actualizacion

--- logica.js
+++ logica.js
@@ -13,20 +13,22 @@
 function mostrar(data){
   var nombre = ""; /* variable para almacenar el nombre o descripcion de la pagina*/
   var link = ""; // guarda la URL de la pagina
+  var fecha = ""; // guardar la fecha de la pagina guardada
   var val = 0; // mira si se tiene que guardar el nombre o la url
 
   for(var i=0; i<data.length;i++){ //recorremos la cadena (archivo)
     if(data[i]!='\n'){ // hasta que acabe la linea
-      if(data[i]=='-'){val=1;} // cambia la validacion para que lea ahora la URL
+      if(data[i]=='-'){val=val+1;} // cambia la validacion para que lea ahora la URL
       else{
         if(val==0){nombre=nombre+data[i];} // si la validacion esta en 0 leera el nombre
-        else{link=link+data[i];} // caso contrario leera  la URL
+        else if(val==1){link=link+data[i];} // si es 1 leera  la URL
+        else{fecha=fecha+data[i];} // o en caso contrario leera la fecha
       }
     }
     else{
       // mostrara los datos recolectados del archivo para poder acceder a ellos
-      $("#info").append("<tr><td>"+nombre+"</td><td>"+"<a href=\""+link+"\">"+link+"</a>"+"</td><td>12/12/12</td></tr>");
-      nombre = link = ""; //receteamos los valores para la nueva linea
+      $("#info").append("<tr><td>"+nombre+"</td><td>"+"<a href=\""+link+"\">"+link+"</a>"+"</td><td>"+fecha+"</td></tr>");
+      nombre = link = fecha = ""; //receteamos los valores para la nueva linea
       val=0;
     }
   }
